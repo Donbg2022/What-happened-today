38f82db37468c369ac504c6a0a9685a863db6c68
Donovan <donovangriffin04@gmail.com>
2022-11-16 15:35:52 -0800

    added eventlistener for birth

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-async function thisDay(){
-  const day = await axios.get(`https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/05/25`)
-  console.log(day)
+const input = document.querySelector('#input')
+const birthBtn = document.querySelector('#birth')
+const deathBtn = document.querySelector('#death')
+const newsBtn = document.querySelector('#news')
+const notableBtn = document.querySelector('#notable')
+let month = ''
+let day = ''
+
+
+//async function used on birth button to display birthdays on date picked
+async function birthDay(e){
+  //prevents page from reloading when form button is pressed
+  e.preventDefault()
+  //slicing the date to get month and day from the string respectedly
+  day = input.value.slice(8,10)
+  month = input.value.slice(5,7)
+  //api call using template literals and axios
+  const dateApi = await axios.get(`https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${month}/${day}`)
+  console.log(dateApi)
 }
 
-thisDay()
+
+birthBtn.addEventListener('click', birthDay)
