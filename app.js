243b9f192da82d4989da147cebc9dd60d5bc1f5b
Donovan <donovangriffin04@gmail.com>
2022-11-16 17:04:56 -0800

    refactored javascript 4 seperate functions into on singular one

--- app.js
+++ app.js
@@ -7,42 +7,56 @@ let month = ''
 let day = ''
 const ul = document.querySelector('#ul')
 
-//async function used on birth button to display birthdays on date picked
-async function birthDay(e){
-  //prevents page from reloading when form button is pressed
+
+
+let buttons = document.querySelector('.buttons')
+
+//event listener to trigger today function
+buttons.addEventListener('click', today)
+
+//async function today
+async function today(e){
+
+  //prevents page from reloading due to 'form' submission
   e.preventDefault()
-  //slicing the date to get month and day from the string respectedly
+
+  //slices pieces of date input for day and month variables
   day = input.value.slice(8,10)
   month = input.value.slice(5,7)
-  //api call using template literals and axios
-  const dateApi = await axios.get(`https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${month}/${day}`)
   
-  //loops through array and displays the names of people born on said day
-  let notedBirths = dateApi.data.births
-  for (let i = 0; i < 20; i++) {
-    let birthList = ul.appendChild(document.createElement('li'))
-    birthList.innerText = notedBirths[i].text
+  //api call to get data
+  const dateApi = await axios.get(`https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${month}/${day}`)
+  console.log(dateApi)
+
+  //global variable to use in if statements
+  let notables = ''
+
+  //series off if statements to check which button was acyually pressed and adds data to user screen
+  if (e.target.innerText === 'BIRTHS'){
+    let notables = dateApi.data.births
+    for (let i = 0; i < 20; i++) {
+    let valList = ul.appendChild(document.createElement('li'))
+    valList.innerText = notables[i].text
+}
+  }else if (e.target.innerText === 'DEATHS') {
+    let notables = dateApi.data.deaths
+    for (let i = 0; i < 20; i++) {
+    let valList = ul.appendChild(document.createElement('li'))
+    valList.innerText = notables[i].text
   }
+}else if (e.target.innerText === 'NEWS EVENTS') {
+  let notables = dateApi.data.events
+  for (let i = 0; i < 20; i++) {
+  let valList = ul.appendChild(document.createElement('li'))
+  valList.innerText = notables[i].text
+}
+}else if(e.target.innerText === 'NOTABLE EVENTS'){
+    let notables = dateApi.data.selected
+    for (let i = 0; i < 20; i++) {
+    let valList = ul.appendChild(document.createElement('li'))
+    valList.innerText = notables[i].text
+  }
+  }
+  
 }
 
-async function deathDay(e){
-  e.preventDefault()
-  day = input.value.slice(8,10)
-  month = input.value.slice(5,7)
-
-  const dateApi = await axios.get(`https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${month}/${day}`)
-  
-  let notedDeaths = dateApi.data.deaths
-  for (let i = 0; i < 20; i++) {
-    let deathList = ul.appendChild(document.createElement('li'))
-    deathList.innerText = notedDeaths[i].text
-  }
-}
-
-
-birthBtn.addEventListener('click', birthDay)
-deathBtn.addEventListener('click', deathDay)
-// newsBtn.addEventListener('click', newsDay)
-// notableBtn.addEventListener('click', notableDay)
-
-console.log(ul)
